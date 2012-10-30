8a2dc12708f455e66f25d6238b91d16a81fd7fee
powder197 <daniel.amy@gmail.com>
2012-10-30 12:15:47 -0400

    Initial Commit

---
+++ config.rb
@@ -0,0 +1,7 @@
+http_path = "/"
+css_dir = "assets/css"
+sass_dir = "assets/css/sass"
+images_dir = "assets/img"
+javascripts_dir = "assets/js"
+relative_assets = true
+Sass::Script::Number.precision = 15
